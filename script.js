c0b126b0ba0aa190abd1697e39d875fec551d6c8
mika <mikagivner123@gmail.com>
2023-01-23 12:39:11 +0200

    promtsSection

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
+let length =prompt("Which password length do you want?");
+
+    
+    while(isNaN(length)){
+        length=prompt("try again. please write a number");
+    }
+    let hasUpperCase=prompt(`Great, your password length is ${length}. now please answer with yes or no-do you want Upper Case in your password?`);
+
+    while(hasUpperCase!=='yes' || hasUpperCase!=='no'){
+        let hasUpperCase=prompt("please answer only with yes or no");
+    }
+    
+    let hasLowerCase=prompt("and what about Lower Case")
+
+
+
+
+
 // promts
 // validate  
 // length number
 // hasUpperCase hasLowwerCAse ....
 
-//call the generator
-// Generate(length, hasUppercase , hasLowercase , hasNumbers, hasSpecial )
-
-// implement the generate()
