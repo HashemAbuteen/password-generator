26a725a1f308bb8ce6e0ee964bdc909a556c0880
Hashem <hashemabualteen@gmail.com>
2023-01-23 11:47:54 +0200

    wrote the plan

---
+++ script.js
@@ -0,0 +1,9 @@
+// promts
+// validate  
+// length number
+// hasUpperCase hasLowwerCAse ....
+
+//call the generator
+// Generate(length, hasUppercase , hasLowercase , hasNumbers, hasSpecial )
+
+// implement the generate()
